83f7d8c3602c5ebafd6c18ccc000d9cdcbc1eed8
Rémi Heredero <remi@heredero.ch>
2021-12-04 23:26:57 +0100

    merci Louis

--- app/src/main/java/com/klagarge/hangman/MainActivity.java
+++ app/src/main/java/com/klagarge/hangman/MainActivity.java
@@ -1,21 +1,27 @@
 package com.klagarge.hangman;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+    private MyView drawingView;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyView myViewCompteur = new MyView(this);
+        drawingView = new MyView(this);
         LinearLayout myLayout1 = (LinearLayout)findViewById(R.id.myView);
-        myLayout1.addView(myViewCompteur);
-        //setContentView(myViewCompteur);
+        myLayout1.addView(drawingView);
+    }
+
+    public void update(View view) {
+        drawingView.state++;
+        drawingView.invalidate();
     }
 }
--- app/src/main/java/com/klagarge/hangman/MyView.java
+++ app/src/main/java/com/klagarge/hangman/MyView.java
@@ -1,5 +1,6 @@
 package com.klagarge.hangman;
 
+import android.util.Log;
 import android.view.View;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -11,50 +12,59 @@ import android.graphics.RectF;
 
 public class MyView extends View
 {
-    //Paint paint = new Paint(Paint.FAKE_BOLD_TEXT_FLAG);
     Paint paint = new Paint(Paint.LINEAR_TEXT_FLAG);
-    //Path curvePath;
-    //Paint textPaint = new Paint(Paint.LINEAR_TEXT_FLAG);
+    public int state = 0;
 
     public MyView(Context context){
         super(context);
     }
 
     @Override
-    protected void onDraw(Canvas canvas)
-    {
-        // draw basic shapes
-
-        paint.setColor(Color.WHITE);
-
+    protected void onDraw(Canvas canvas){
         float scale = 3.0F;
-
-        //jambes de la potence
-        canvas.drawLine(110 * scale, 210 * scale, 120 * scale, 190 * scale, paint);
-        canvas.drawLine(130 * scale, 210 * scale, 120 * scale, 190 * scale, paint);
-
-        //potence
-        canvas.drawLine(120 * scale, 190 * scale, 120 * scale, 100 * scale, paint);
-
-        //bras potence
-        canvas.drawLine(120 * scale, 100 * scale, 180 * scale, 100 * scale, paint);
-
-        //fil potence
-        canvas.drawLine(180 * scale, 100 * scale, 180 * scale, 110 * scale, paint);
-
-        //tête
+        paint.setColor(Color.WHITE);
         paint.setStyle(Paint.Style.STROKE);
-        canvas.drawCircle(180 * scale, 120 * scale, 10 * scale, paint);
 
-        //corps
-        canvas.drawLine(180 * scale, 130 * scale, 180 * scale, 170 * scale, paint);
+        if (state > 0) {
+            //jambes de la potence
+            canvas.drawLine(110 * scale, 210 * scale, 120 * scale, 190 * scale, paint);
+            canvas.drawLine(130 * scale, 210 * scale, 120 * scale, 190 * scale, paint);
+        }
 
-        //jambes
-        canvas.drawLine(180 * scale, 170 * scale, 170 * scale, 190 * scale, paint);
-        canvas.drawLine(180 * scale, 170 * scale, 190 * scale, 190 * scale, paint);
+        if (state > 1) {
+            //potence
+            canvas.drawLine(120 * scale, 190 * scale, 120 * scale, 100 * scale, paint);
+        }
 
-        //bras
-        canvas.drawLine(170 * scale, 150 * scale, 190 * scale, 150 * scale, paint);
+        if (state > 2) {
+            //bras potence
+            canvas.drawLine(120 * scale, 100 * scale, 180 * scale, 100 * scale, paint);
+        }
 
+        if (state > 3) {
+            //fil potence
+            canvas.drawLine(180 * scale, 100 * scale, 180 * scale, 110 * scale, paint);
+        }
+
+        if (state > 4) {
+            //tête
+            canvas.drawCircle(180 * scale, 120 * scale, 10 * scale, paint);
+        }
+
+        if (state > 5) {
+            //corps
+            canvas.drawLine(180 * scale, 130 * scale, 180 * scale, 170 * scale, paint);
+        }
+
+        if (state > 6) {
+            //jambes
+            canvas.drawLine(180 * scale, 170 * scale, 170 * scale, 190 * scale, paint);
+            canvas.drawLine(180 * scale, 170 * scale, 190 * scale, 190 * scale, paint);
+        }
+
+        if (state > 7) {
+            //bras
+            canvas.drawLine(170 * scale, 150 * scale, 190 * scale, 150 * scale, paint);
+        }
     }
 }
